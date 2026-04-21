9dbe4dec5177c9b763ff83c6c2291263b99a6e46
Csorba Péter <csosaka21@gmail.com>
2026-04-21 13:58:03 +0200

    fejléc

--- src/main/java/com/example/AboutController.java
+++ src/main/java/com/example/AboutController.java
@@ -1,3 +1,12 @@
+/*
+* File: AboutController.java
+* Author: Csorba Péter
+* Copyright: 2026, Csorba Péter
+* Group: Szoft I/N
+* Date: 2026-04-21
+* Github: https://github.com/Gunkhu
+* Licenc: MIT
+*/
 package com.example;
 
 import javafx.event.ActionEvent;
